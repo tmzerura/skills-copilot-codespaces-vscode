ea229bf244af021e4be2f1b597bd3401f83b2702
tmzerura <137553988+tmzerura@users.noreply.github.com>
2023-08-11 04:59:55 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/member/member.html',
+        controller: 'MemberController',
+        controllerAs: 'memberCtrl'
+    };
+}
